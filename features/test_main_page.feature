c4af9a8cb8d3f4b344a1c7dd41a5de686eecc326
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-11-12 11:23:03 -0700

    added parametrization from .json

--- features/test_main_page.feature
+++ features/test_main_page.feature
@@ -1,7 +1,12 @@
 Feature: Main page form submission
 
-  Scenario: Successful form submission
+  Scenario Outline: Successful form submission
     Given I open the main page
-    When I fill the form with name "Test", email "test@test.com", message "Test message", and request type "Личный"
+    When I fill the form with name "<name>", email "<email>", message "<message>", and request type "<request_type>"
     And I submit the form
     Then I should see the success message "Спасибо! Ваш запрос уже получен.Я свяжусь с вами по указанному email в течение 3 дней или ранее, если я смогу что-то предложить в ответ."
+
+  Examples:
+    | name      | email                    | message               | request_type   |
+    | Personal  | personal@test.com        | Personal test message | Личный         |
+    |           | commercial@commercial.com| commercial message    | Коммерческий   |
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,12 +1,12 @@
 import pytest
 from playwright.sync_api import expect
 from pages.main_page import MainPage
+from utils.test_data_loader import load_test_data
 
-@pytest.mark.parametrize("name, email, message, request_type", [
-    ("Personal", "personal@test.com", "Personal test message", "Личный"),
-    ("", "commercial@commercial.com", "commercial message", "Коммерческий")])
-def test_main_page(page, name, email, message, request_type):
+
+@pytest.mark.parametrize("data", load_test_data("test_data/main_page.json"))
+def test_main_page(page, data):
     page = MainPage(page)
-    page.fill_form(name=name, email=email, message=message, request_type=request_type)
+    page.fill_form(data["name"], data["email"], data["message"], data["request_type"])
     page.submit_form()
     expect(page.success_message).to_have_text("Спасибо! Ваш запрос уже получен.Я свяжусь с вами по указанному email в течение 3 дней или ранее, если я смогу что-то предложить в ответ.")
