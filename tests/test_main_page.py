81245c89043e1c83339fa06dadf82edf77ee9e6a
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-11-12 10:50:26 -0700

    paramertrized positive tests

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,14 +1,12 @@
+import pytest
 from playwright.sync_api import expect
 from pages.main_page import MainPage
 
-
-def test_main_page(page):
+@pytest.mark.parametrize("name, email, message, request_type", [
+    ("Personal", "personal@test.com", "Personal test message", "Личный"),
+    ("", "commercial@commercial.com", "commercial message", "Коммерческий")])
+def test_main_page(page, name, email, message, request_type):
     page = MainPage(page)
-    page.fill_form(
-        name="Test", 
-        email="test@test.com",
-        message="Test message",
-        request_type="Личный",
-        )
+    page.fill_form(name=name, email=email, message=message, request_type=request_type)
     page.submit_form()
     expect(page.success_message).to_have_text("Спасибо! Ваш запрос уже получен.Я свяжусь с вами по указанному email в течение 3 дней или ранее, если я смогу что-то предложить в ответ.")
