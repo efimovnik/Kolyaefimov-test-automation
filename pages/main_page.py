677fa4f8435f0cc72e601b45d3a54941c2911124
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-11-12 09:57:43 -0700

    tested submission form

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,21 @@
+from playwright.sync_api import Page
+
 class MainPage:
-    def __init__(self, page):
+    def __init__(self, page: Page):
         self.page = page
         self.name = page.get_by_label("Имя")
+        self.email = page.get_by_label("Email(required)")
+        self.message = page.get_by_placeholder("Чем я могу вам помочь?")
+        self.request_type = page.get_by_label("Тип запроса")
+        self.submit_button = page.get_by_role("button", name="Отправить")
+        self.success_message = page.locator(".form-submission-text")
+
+    def fill_form(self, name: str, email: str, message: str, request_type: str = None):
+        self.name.fill(name)
+        self.email.fill(email)
+        self.message.fill(message)
+        if request_type:
+            self.request_type.select_option(request_type)
+
+    def submit_form(self):
+        self.submit_button.click()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -5,4 +5,11 @@ from pages.main_page import MainPage
 
 def test_main_page(set_up):
     page = MainPage(set_up)
-    expect(page.name).to_have_text("")
+    page.fill_form(
+        name="Test", 
+        email="test@test.com",
+        message="Test message",
+        request_type="Личный",
+        )
+    page.submit_form()
+    expect(page.success_message).to_have_text("Спасибо! Ваш запрос уже получен.Я свяжусь с вами по указанному email в течение 3 дней или ранее, если я смогу что-то предложить в ответ.")
