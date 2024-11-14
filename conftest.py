2c7a7f6f732f947f028cedc45291a3a3963c77c4
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-11-13 20:07:31 -0700

    added slowmo to avoid captcha validation during test execution

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from pages.main_page import MainPage
 @pytest.fixture(scope="function")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=True, slow_mo=50)
         yield browser
         browser.close()
 
